aa6f247faeb11f962680a6db7d77cf6762e453c4
Miguel Ángel <mdiasan28@gmail.com>
2020-02-07 13:48:57 +0100

    matematicas

---
+++ src/ejerciciosTema6/Ejercicios_Wrappers.java
@@ -0,0 +1,46 @@
+package ejerciciosTema6;
+
+public class Ejercicios_Wrappers {
+
+	public static void main(String[] args) {
+		ej1();
+
+	}
+
+	public static void ej1(){
+
+		// Wrapper para el tipo de datos "Byte"
+		System.out.println("Valor máximo para un Byte: " + Byte.MAX_VALUE);
+		System.out.println("Valor mínimo para un Byte: " + Byte.MIN_VALUE);
+		System.out.println("Tamaño en bits complemento a 2 para Byte: " + Byte.SIZE);
+		
+		// Wrapper para el tipo de datos "int"
+		System.out.println("Valor máximo para un Integer: " + Integer.MAX_VALUE);
+		System.out.println("Valor mínimo para un Integer: " + Integer.MIN_VALUE);
+		System.out.println("Tamaño en bits complemento a 2 para Integer: " + Integer.SIZE);
+
+		// Wrapper para el tipo de datos "long"
+		System.out.println("\nValor máximo para un Long: " + Long.MAX_VALUE);
+		System.out.println("Valor mínimo para un Long: " + Long.MIN_VALUE);
+		System.out.println("Tamaño en bits complemento a 2 para Long: " + Long.SIZE);
+
+		// Wrapper para el tipo de datos "short"
+		System.out.println("\nValor máximo para un Short: " + Short.MAX_VALUE);
+		System.out.println("Valor mínimo para un Short: " + Short.MIN_VALUE);
+		System.out.println("Tamaño en bits complemento a 2 para Short: " + Short.SIZE);
+
+		// Wrapper para el tipo de datos "float"
+		System.out.println("\nValor máximo para un Float: " + Float.MAX_VALUE);
+		System.out.println("Valor mínimo para un Float: " + Float.MIN_VALUE);
+		System.out.println("Tamaño en bits complemento a 2 para Float: " + Float.SIZE);
+
+		// Wrapper para el tipo de datos "double"
+		System.out.println("\nValor máximo para un Double: " + Double.MAX_VALUE);
+		System.out.println("Valor mínimo para un Double: " + Double.MIN_VALUE);
+		System.out.println("Tamaño en bits complemento a 2 para Double: " + Double.SIZE);
+	}
+
+
+}
+
+
